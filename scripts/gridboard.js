a563e9b97d10b8111a66c75ce5ad1f9fcf4d059d
bverryt <bjorn.verryt@gmail.com>
2012-10-16 10:31:10 +0200

    syntax fixes

--- scripts/gridboard.js
+++ scripts/gridboard.js
@@ -1,19 +1,20 @@
-﻿// TODO: add residential buildings ('receivers')
-// TODO: add buildings as json
+﻿// TODO: use prototyping on the building plans
+// TODO: optimize the .each() calls in getOccupiedTiles/getAffectedTiles
+// TODO: add residential buildings ('receivers')
 // TODO: allow save/load of board + inventory
 // TODO: let user add items to inventory
 // TODO: use different colours (decorations/attractions)
 // TODO: re-write as a generic board game plugin
 
 /* CONSTANTS */
-var NUM_OF_ROWS = 20, NUM_OF_COLS = 30;
+var NUM_OF_ROWS = 20, NUM_OF_COLS = 30; TILE_SIZE = 20;
 
 /* GLOBALS */
-var lookup = [], logcounter = 1;
-
-/* INIT */
+var tileLookup = [], logCounter = 1;
 
 $(init);
+
+//<editor-fold desc="INIT">
 
 function init() {
 	$("body").disableSelection();
@@ -24,32 +25,46 @@ function init() {
 
 function initBoard() {
 	var board = $("#board");
-	createSpots(board, NUM_OF_ROWS, NUM_OF_COLS);
+    for (var y = 1; y <= NUM_OF_ROWS; y++) {
+        var row = $("<tr/>").addClass("row").appendTo(board);
+        for (var x = 1; x <= NUM_OF_COLS; x++) {
+            createTile(x, y).appendTo(row);
+        }
+    }
 }
 
 function initInventory() {
 	var board = $("#board");
 	var inventory = $("#inventory");
-	var br = "<br/>";
+    var br = "<br/>";
 
-	function repeat(times, fn) { for (var i = 0; i < times; i++) fn(); }
-	repeat(15, function () { createBuilding(1, 1, 5, 2); }); inventory.append(br);
-	repeat(5, function () { createBuilding(2, 2, 10, 2); });
-	repeat(2, function () { createBuilding(3, 2, 10, 2); });
-	repeat(2, function () { createBuilding(1, 2, 10, 2); }); inventory.append(br);
-	repeat(3, function () { createBuilding(3, 3, 25, 3); });
-	repeat(3, function () { createBuilding(2, 3, 25, 3); }); inventory.append(br);
-	repeat(2, function () { createBuilding(4, 4, 50, 3); }); inventory.append(br);
+	appendBuildings(10, {width:1, height:1, bonus:5,  range:2, type:'decoration'});  inventory.append(br);
+	appendBuildings(3,  {width:2, height:2, bonus:10, range:2, type:'decoration'});
+	appendBuildings(2,  {width:3, height:2, bonus:10, range:2, type:'decoration'});  inventory.append(br);
+	appendBuildings(2,  {width:3, height:3, bonus:25, range:3, type:'decoration'});
+	appendBuildings(3,  {width:2, height:3, bonus:25, range:3, type:'decoration'});  inventory.append(br);
+	appendBuildings(5,  {width:2, height:2, bonus:0,  range:2, type:'residential'}); inventory.append(br);
+	appendBuildings(5,  {width:2, height:2, bonus:0,  range:2, type:'residential'}); inventory.append(br);
 
 	inventory.droppable({ drop: handleBuildingRestack, tolerance: 'fit' });
 	inventory.height(board.height() - 22);
 }
 
+function appendBuildings(amount, plan) {
+    var inventory = $("#inventory");
+    for (var i = 0; i < amount; i++) {
+            var building = createBuilding(plan);
+            var id = "building" + (inventory.find(".building").length + 1);
+            building.attr("id", id);
+            building.appendTo(inventory);
+    }
+}
+
 function initBuildingEdit() {
 	var buildingEdit = $("<form/>").attr("id", "buildingEdit").appendTo("#inventory");
-    buildingEdit.append("<label>value</label>").append(createTextInput("value")).append("<br/>");
-	buildingEdit.append("<label>range</label>").append(createTextInput("range")).append("<br/>");
-	buildingEdit.append("<label>name</label>").append(createTextInput("name")).append("<br/>");
+    buildingEdit.append("<label>bonus</label>").append(createTextInput("bonus")).append("<br/>");
+    buildingEdit.append("<label>range</label>").append(createTextInput("range")).append("<br/>");
+    buildingEdit.append("<label>name</label>").append(createTextInput("name")).append("<br/>");
 
 	buildingEdit.dialog({
 		height: 120	, width: 250,
@@ -57,92 +72,107 @@ function initBuildingEdit() {
 		title: "edit building"
 	});
 }
+//</editor-fold>
 
-function createTextInput(name){
-    return $("<input/>").attr("type", "text").attr("name", name);
+//<editor-fold desc="CREATORS">
+
+function createTile(x, y) {
+    var tile = $("<td/>").addClass("tile");
+    tile.width(TILE_SIZE).height(TILE_SIZE);
+
+    tile.droppable({
+        accept:".building", tolerance:"pointer", hoverClass:"hovered",
+        over:handleTileOver, drop:handleBuildingPlacement
+    });
+
+    if (tileLookup == null) tileLookup = new Array(NUM_OF_COLS);
+    if (tileLookup[x] == null) tileLookup[x] = new Array(NUM_OF_ROWS);
+    tileLookup[x][y] = null;
+    return tile;
 }
 
-function createSpots(board, rows, cols) {
-	for (var y = 1; y <= rows; y++) {
-		var row = $("<tr/>").addClass("row").appendTo(board);
-		for (var x = 1; x <= cols; x++) {
+function createBuilding(plan){
+    if (!plan.bonus) plan.bonus = 0;
+    if (!plan.range) plan.range = 2;
+    if (!plan.type) plan.type = 'decoration';
+    if (!plan.width) plan.width = 1;
+    if (!plan.height) plan.height = 1;
+    if (!plan.value) plan.value = 0;
 
-			var tile = $("<td/>").addClass("tile").appendTo(row);
-			tile.droppable({
-                accept:".building", tolerance:"pointer", hoverClass:"hovered",
-                over:handleSpotOver, drop:handleBuildingPlacement
-            });
+	var building = $("<div/>").addClass("building");
 
-			if (lookup == null) lookup = new Array(NUM_OF_COLS);
-			if (lookup[x] == null) lookup[x] = new Array(NUM_OF_ROWS);
-			lookup[x][y] = null;
-		}
-	}
-}
+	building.text(plan.bonus);
+    building.attr("title", plan.bonus + "x" + plan.range);
+    building.addClass(plan.type);
 
-function createBuilding(width, height, value, range) {
-	var inventory = $("#inventory");
-	var id = "building" + (inventory.find(".building").length + 1);
+	building.data("range", plan.range);
+    building.data("bonus", plan.bonus);
+    building.data("name", "");
+    building.data("width", plan.width);
+    building.data("height", plan.height);
+    building.data("value", plan.value);
 
-	var building = $("<div/>").addClass("building").attr("id", id).appendTo(inventory);
-	building.text(value).attr("title", value + "x" + range);
-	building.data("range", range).data("value", value).data("name", "").data("width", width).data("height", height);
-	building.draggable({ start: handleDragStart, revert: "invalid", inventory: ".building", cursorAt: { left: 5, top: 5} });
+    building.draggable({ start:handleDragStart, revert:"invalid", inventory:".building", cursorAt:{ left:5, top:5} });
 	building.dblclick(handleBuildingEdit);
 
-	building.width(building.width() * width + (width - 1));
-	building.height(building.height() * height + (height - 1));
-	return building;
+	building.width(TILE_SIZE * plan.width + (plan.width - 1));
+	building.height(TILE_SIZE * plan.height + (plan.height - 1));
+
+    return building;
 }
 
-/* EVENT HANDLERS */
+//</editor-fold>
+
+//<editor-fold desc="EVENT HANDLERS">
 
 function handleDragStart(event, ui) {
     $("#board").find("td.tile").draggable("option", "accept", ".building"); // reset all tiles to accept all buildings
 }
 
-function handleSpotOver(event, ui) {
+function handleTileOver(event, ui) {
 	var tile = $(this);
 	var building = ui.draggable;
-	if (!validateSpot(tile, building)) { // if invalid
-		var selector = ".building:not(#" + $(building).attr("id") + ")"; // don't accept the current building
+	if (!validateTile(tile, building)) { // if invalid
+		var selector = ".building:not(#" + $(building).attr("id") + ")"; // don't deny the current building
 		tile.droppable("option", "accept", selector).removeClass("hovered");
 	}
 }
 
 function handleBuildingPlacement(event, ui) {
 	var building = ui.draggable;
-	var oldSpot = $(building).data("tile");
-	var newSpot = $(this);
+	var oldTile = $(building).data("tile");
+	var newTile = $(this);
 
-	if (oldSpot != null) cleanupBuilding(oldSpot, building); // cleanup old tile
-	placeBuilding(newSpot, building); // place on new tile	
+	if (oldTile != null) cleanupBuilding(oldTile, building); // cleanup old tile
+	placeBuilding(newTile, building); // place on new tile
 	building.position({ of: $(this), my: "left top", at: "left top" });
 }
 
 function handleBuildingRestack(event, ui) {
 	var building = ui.draggable;
-	var oldSpot = $(building.data("tile"));
-	cleanupBuilding(oldSpot, building); // remove from old tile
+	var oldTile = $(building.data("tile"));
+	cleanupBuilding(oldTile, building); // remove from old tile
 }
 
 function handleBuildingEdit(event) {
 	var building = $(this);
 	var buildingEdit = $("#buildingEdit");
 
-	buildingEdit.enter(function () { editValue(buildingEdit, building); });
-	buildingEdit.find("input[name=value]").val($(building).data("value")).focus(function () { this.select(); });
+	buildingEdit.enter(function () { editBuilding(buildingEdit, building); });
+	buildingEdit.find("input[name=bonus]").val($(building).data("bonus")).focus(function () { this.select(); });
 	buildingEdit.find("input[name=range]").val($(building).data("range"));
 	buildingEdit.find("input[name=name]").val($(building).data("name"));
 	buildingEdit.dialog("open");
-}	
+}
 
-/* ACTIONS */
+//</editor-fold>
 
-function editValue(buildingEdit, building) {
-	var oldValue = $(building).data("value");
-	var newValue = parseInt(buildingEdit.find("input[name=value]").val());
-	$(building).data("value", newValue).text(newValue);
+//<editor-fold desc="ACTIONS">
+
+function editBuilding(buildingEdit, building) {
+	var oldBonus = $(building).data("bonus");
+	var newBonus = parseInt(buildingEdit.find("input[name=bonus]").val());
+	$(building).data("bonus", newBonus).text(newBonus);
 
 	var oldRange = $(building).data("range");
 	var newRange = parseInt(buildingEdit.find("input[name=range]").val());
@@ -154,14 +184,14 @@ function editValue(buildingEdit, building) {
 
 	var tile = $(building).data("tile");
 	if (tile != null) {
-		cleanupBuilding(tile, building, oldValue, oldRange);
-		placeBuilding(tile, building, newValue, newRange);
+		cleanupBuilding(tile, building, oldBonus, oldRange);
+		placeBuilding(tile, building, newBonus, newRange);
 	}
 
 	$(buildingEdit).dialog("close");
 }
 
-function validateSpot(tile, building) {
+function validateTile(tile, building) {
 	var width = $(building).data("width");
 	var height = $(building).data("height");
 	if (width == 1 && height == 1) return true;
@@ -173,68 +203,69 @@ function validateSpot(tile, building) {
 	var id = $(building).attr("id");
 	for (var x = coords.x; x < coords.x + width; x++)
 		for (var y = coords.y; y < coords.y + height; y++) 
-			if (lookup[x][y] != null && lookup[x][y] != id) return false;
+			if (tileLookup[x][y] != null && tileLookup[x][y] != id) return false;
 
 	return true;
 }
 
-function placeBuilding(tile, building, value, range) {
-	processBuildingChange(tile, building, value, range, tileActivate, function (a, b) { return a + b; });
+function placeBuilding(tile, building, bonus, range) {
+	processBuildingPlacement(tile, building, bonus, range, tileActivate, function (a, b) { return a + b; });
 }
 
-function cleanupBuilding(tile, building, value, range) {
-	processBuildingChange(tile, building, value, range, tileDeactivate, function (a, b) { return a - b; });
+function cleanupBuilding(tile, building, bonus, range) {
+	processBuildingPlacement(tile, building, bonus, range, tileDeactivate, function (a, b) { return a - b; });
 }
 
-function processBuildingChange(tile, building, value, range, tileAction, valueCalc) {
-
-
+function processBuildingPlacement(tile, building, bonus, range, tileAction, bonusCalc) {
 	var width = $(building).data("width");
 	var height = $(building).data("height");
-	if (value == null) value = $(building).data("value");
+	if (bonus == null) bonus = $(building).data("bonus");
 	if (range == null) range = $(building).data("range");
 
 	// occupied tiles = tiles covered by the building itself
-	var occupiedSpots = getOccupiedSpots(tile, width, height);
-	$(occupiedSpots).each(function () { tileAction(tile, this, building); });
+	var occupiedTiles = getOccupiedTiles(tile, width, height);
+	$(occupiedTiles).each(function () { tileAction(tile, this, building); });
 
 	// affected tiles = tiles within range
-	var affectedSpots = getAffectedSpots(tile, range, width, height);
-	$(affectedSpots).each(function () { updateSpotValue(this, value, valueCalc); });
+	var affectedTiles = getAffectedTiles(tile, range, width, height);
+	$(affectedTiles).each(function () { updateTileBonus(this, bonus, bonusCalc); });
 }
 
-function updateSpotValue(tile, value, valueCalc) {
+function updateTileBonus(tile, bonus, bonusCalc) {
 	var coords = getCoords(tile);
-	var oldValue = $(tile).data("value");
-	if (oldValue == null) oldValue = 0;
-	var newValue = valueCalc(oldValue, value);
-	$(tile).data("value", newValue);
+	var oldBonus = $(tile).data("bonus");
+	if (oldBonus == null) oldBonus = 0;
+	var newBonus = bonusCalc(oldBonus, bonus);
+	$(tile).data("bonus", newBonus);
 	$(tile).empty();
-	if (newValue > 0) $("<div/>").appendTo(tile).text(newValue);
+	if (newBonus > 0) $("<div/>").appendTo(tile).text(newBonus);
 }
 
-function tileDeactivate(originSpot, tile, building) {
+function tileDeactivate(originTile, tile, building) {
 	$(building).data("tile", null);
 	$(tile).removeClass("active");
 	var coords = getCoords(tile);
-	lookup[coords.x][coords.y] = null;
+	tileLookup[coords.x][coords.y] = null;
 }
 
-function tileActivate(originSpot, tile, building) {
-	$(building).data("tile", originSpot);
+function tileActivate(originTile, tile, building) {
+	$(building).data("tile", originTile);
 	$(tile).addClass("active");
 	var coords = getCoords(tile);
-	lookup[coords.x][coords.y] = building.attr("id");
+	tileLookup[coords.x][coords.y] = building.attr("id");
 }
+//</editor-fold>
 
-/* == HELPERS == */
-
-
+//<editor-fold desc="HELPERS">
 
 //noinspection JSUnusedGlobalSymbols
 function writeLog(message, p1, p2, p3, p4) {
 	var log = message.replace("$1",p1).replace("$2",p2).replace("$3",p3).replace("$4",p4);
-	console.log(logcounter++ + ": " + log);
+	console.log(logCounter++ + ": " + log);
+}
+
+function createTextInput(name){
+    return $("<input/>").attr("type", "text").attr("name", name);
 }
 
 function getCoords(tile) {
@@ -244,38 +275,42 @@ function getCoords(tile) {
 	return { x: x, y: y };
 }
 
-function getOccupiedSpots(originSpot, width, height) {
-	var occupiedSpots = [];
-	var origin = getCoords(originSpot);
+function getOccupiedTiles(originTile, width, height) {
+	var occupiedTiles = [];
+	var origin = getCoords(originTile);
 	// TODO: find a better occupied/affected test than scanning the entire board
 	$("#board").find("td").each(function () {
 		var coords = getCoords(this);
 		if (coords.x >= origin.x && coords.x < origin.x + width)
 			if (coords.y >= origin.y && coords.y < origin.y + height)
-				occupiedSpots.push(this);
+				occupiedTiles.push(this);
 	});
-	return occupiedSpots;
+	return occupiedTiles;
 }
 
-function getAffectedSpots(originSpot, range, width, height) {
-	var affectedSpots = [];
-	var origin = getCoords(originSpot);
+function getAffectedTiles(originTile, range, width, height) {
+	var affectedTiles = [];
+	var origin = getCoords(originTile);
 	$("#board").find("td").each(function () {
 		var coords = getCoords(this);
 		if (coords.x >= origin.x - range && coords.x < origin.x + range + width)
 			if (coords.y >= origin.y - range && coords.y < origin.y + range + height) 
-				affectedSpots.push(this);
+				affectedTiles.push(this);
 	});
-	return affectedSpots;
+	return affectedTiles;
 }
 
+//</editor-fold>
+
+//<editor-fold desc="EXTENSIONS">
 // new extension: "enter". Used as a shortcut to the keypress handler, but only for the enter key. Will only work once, then unbind.
-(function ($) {	
+(function ($) {
 	$.fn.enter = function (handler) {
-		return $(this).keypress(function (event) { 
-			if (event.which == 13) { event.preventDefault(); handler(); $(this).unbind(event); } 
+		return $(this).keypress(function (event) {
+			if (event.which == 13) { event.preventDefault(); handler(); $(this).unbind(event); }
 			});
 	}}
 )(jQuery);
+//</editor-fold>
 
 
